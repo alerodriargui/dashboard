d1e413ce30b3e0d769ca6d6abaaab364eb710608
Alejandro Rodríguez <alerodriargui@gmail.com>
2023-10-30 18:16:12 +0100

    update

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, render_template, send_from_directory
+import pandas as pd
+
+app = Flask(__name__)
+
+# Cargar los datos
+pd.set_option('display.max_columns', 500)
+df = pd.read_csv('universal_top_spotify_songs.csv')
+
+# Ruta para servir archivos estáticos
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+@app.route('/')
+def index():
+    return render_template('adiiu.html')
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    # Convierte el DataFrame a un diccionario JSON
+    selected_columns = df[['name', 'popularity']]
+
+    # Convierte el subconjunto del DataFrame a un diccionario JSON
+    data = selected_columns.head(30).to_dict(orient='records')
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
--- data.py
+++ data.py
@@ -3,10 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sketch 
-from dataprep.eda import plot, plot_correlation
 pd.set_option('display.max_columns', 500)
-import plotly.express as px
 
 
-df = pd.read_csv('/kaggle/input/top-spotify-songs-in-73-countries-daily-updated/universal_top_spotify_songs.csv', parse_dates=['snapshot_date', 'album_release_date'])
+df = pd.read_csv('universal_top_spotify_songs.csv')
 df.info()
