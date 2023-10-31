33ef35c0930f3609ced29f69ac242502a20c3213
alerodriargui <alerodriargui@gmail.com>
2023-10-31 12:13:26 +0100

    format

--- data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
-pd.set_option('display.max_columns', 500)
-
-
-df = pd.read_csv('universal_top_spotify_songs.csv')
-df.info()
