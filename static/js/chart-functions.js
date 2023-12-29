1d66f8a0a7599cf9603108a1d7c571595476a358
Alejandro Rodríguez <alerodriargui@gmail.com>
2023-12-29 13:19:01 +0100

    update

---
+++ static/js/chart-functions.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    // Funcion para mostrar un gráfico en específico o todos
+    $("#chart-select").change(function() {
+      var selectedChart = $(this).val();
+      $(".chart-containerA, .chart-containerB, .chart-containerC, .chart-containerD").hide();
+      switch (selectedChart) {
+        case "todos":
+          $(".chart-containerA, .chart-containerB, .chart-containerC, .chart-containerD").fadeIn('slow');
+          break;
+        case "grafica1":
+          $(".chart-containerA").fadeIn('slow');
+          break;
+        case "grafica2":
+          $(".chart-containerB").fadeIn('slow');
+          break;
+        case "grafica3":
+          $(".chart-containerC").fadeIn('slow');
+          break;
+          case "grafica4":
+          $(".chart-containerD").fadeIn('slow');
+          break;
+        default:
+          break;
+      }
+    });
+  });
